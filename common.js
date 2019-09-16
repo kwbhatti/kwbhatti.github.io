3f084c0c63c395c819c7a206607c056f9421d787
kwbhatti <khurram.waleed.bhatti@gmail.com>
2019-09-16 14:39:13 -0400

    updated to include back to top button for the failures

--- common.js
+++ common.js
@@ -2,3 +2,9 @@ function goToIndicator(){
     var link = document.getElementById("indicatorOptions").value;
     location.href= link;
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
